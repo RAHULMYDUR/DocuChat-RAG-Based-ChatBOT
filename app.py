1e90125b1586256ff00b4f103f71727f06990531
RAHUL M <123269117+RAHULMYDUR@users.noreply.github.com>
2024-07-25 17:11:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import streamlit as st
+
+# Try to import functions from retrieval_response
+try:
+    from retrieval_response import retrieve_relevant_chunks, generate_response
+except ImportError as e:
+    st.error(f"ImportError: {e}")
+    st.stop()
+
 from file_handler import extract_text_from_pdf
 from processing import chunk_documents, vectorize_chunks, store_vectors_in_faiss
-from retrieval_response import retrieve_relevant_chunks, generate_response
 
 # Define your API key here
 api_key = "AIzaSyCzdCOyd-7os-SRgbEolxtwEEgYYkjKpsM"
